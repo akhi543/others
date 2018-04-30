8a1875b3bebcd2fe752f51bbb0ef0e91f8356b8a
Akhilesh Gulati <agulati235@gmail.com>
2018-05-01 02:46:28 +0530

    Create openMdWords.py

---
+++ openMdWords.py
@@ -0,0 +1,22 @@
+# open md words
+
+a = ["trite",
+"hegemony",
+"ravenous",
+"arcane",
+"munificent",
+"garrulous",
+"prolific",
+"taciturn",
+"sycophant",
+"sporadic",
+"subversive"]
+
+import webbrowser, time
+
+pre = "https://mnemonicdictionary.com/word/"
+
+for w in a:
+	print "opening " + pre + w
+	webbrowser.open(pre + w)
+	time.sleep(1)
